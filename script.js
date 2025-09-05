bb120f2f108312cc10d1f362ed19f0e25650cdaa
March.Anastasiia <nastya.marchenko00@gmail.com>
2025-09-05 11:41:15 +0300

    Доделаны модалкди десктопа и мобилки

--- script.js
+++ script.js
@@ -21,22 +21,47 @@ body.addEventListener("click", () => {
 });
 
 
-const popup = document.querySelector('.pop-up');
-const bannerButton = document.querySelector('.top-banner_button'); // Кнопка, чтобы открыть
-const crossModal = document.querySelector('.pop-up_cross');
+const popup = document.querySelector(".pop-up");
+const bannerButton = document.querySelector(".top-banner_button"); // Кнопка, чтобы открыть
+const crossModal = document.querySelector(".pop-up_cross");
 
 if (bannerButton) {
-    bannerButton.addEventListener('click', () => {
-        popup.classList.add('active');
+    bannerButton.addEventListener("click", () => {
+        popup.classList.add("active");
     });
 }
 
-crossModal.addEventListener('click', () => {
-    popup.classList.remove('active');
+crossModal.addEventListener("click", () => {
+    popup.classList.remove("active");
 });
 
-popup.addEventListener('click', (e) => {
+popup.addEventListener("click", (e) => {
     if (e.target === popup) {
-        popup.classList.remove('active');
+        popup.classList.remove("active");
+    }
+});
+
+
+const form = document.querySelector(".pop-up_form");
+const emailInput = document.querySelector(".form_input");
+const emailError = document.querySelector(".form_error");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    if (!emailInput.checkValidity()) {
+        emailInput.classList.add("form_input__error");
+        emailError.style.display = "inline";
+        emailInput.blur();
+    } else {
+        emailError.style.display = "none";
+        emailInput.classList.remove("form_input__error");
+    }
+});
+
+emailInput.addEventListener("input", function () {
+    if (emailInput.checkValidity()) {
+        emailInput.classList.remove("form_input__error");
+        emailError.style.display = "none";
     }
 });
